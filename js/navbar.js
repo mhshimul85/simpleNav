567109415945463913a06c206fd6c575324e2b16
mhshimul85 <ishemul85@gmail.com>
2018-12-25 01:37:40 +0600

    First commit for simple nav

---
+++ js/navbar.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  $('.navbar-icon').click(function(){
+    $(this).toggleClass('change');
+    $('nav > ul').toggleClass('mh-show');
+  });
+
+  $('li:has(ul)').children('a').append('<i></i>');
+    
+  $('li > a').each(function(){
+    $(this).click(function(){
+      $(this).next().toggleClass('mh-show');
+      $(this).children().toggleClass('mh-up');
+    });    
+  });
+});
